7071e08ffd33b1026ea53f9271e8a68d7882c5d4
krishnanands17 <76091927+krishnanands17@users.noreply.github.com>
2021-06-17 22:10:05 +0530

    Update symetricC1.java

--- symetricC1.java
+++ symetricC1.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
-class Symetric{
+class Sym
+{
 	int a[][];
 	int x,y;
-	Symetric(int x,int y){
+	Sym(int x,int y)
+	{
 		this.x=x;
 		this.y=y;
 	}
-	void read(){
-		System.out.println("Enter the elements");
+	void get()
+	{
+		System.out.println("ENTER YOUR DATA:");
 		Scanner sc=new Scanner(System.in);
 		for (int i=0;i<x;i++){
 			for (int j=0;j<y;j++){
@@ -15,7 +18,8 @@ class Symetric{
 			}
 		}
 	}
-	void check(){
+	void ch()
+	{
 		int flag=0;
 		for (int i=0;i<x;i++){
 			for (int j=0;j<y;j++){
@@ -24,22 +28,27 @@ class Symetric{
 					break;
 				}
 			}
-		}if(flag==1){
+		}
+		if(flag==1)
+		{
 			System.out.println("This is not symetric");
-		}else{
+		}
+		else
+		{
 			System.out.println("This is Symetric");
 		}
 	}
 }
-class newsymetric{
+class newSym
+{
 	public static void main(String ab[]){
 		System.out.println("Enter how many rows and coloumns");
 		Scanner sc=new Scanner(System.in);
 		int j=sc.nextInt();
 		int k=sc.nextInt();
-		Symetric m1=new Symetric(j,k);
+		Sym m1=new Sym(j,k);
 		m1.a=new int[j][k];
-		m1.read();
-		m1.check();
+		m1.get();
+		m1.ch();
 	}
 }
