3fd168c88a591876bac07acae31c2f07e68a7cf3
krishnanands17 <76091927+krishnanands17@users.noreply.github.com>
2021-09-08 09:30:00 +0530

    Update CO3pg6.java

--- CO3/CO3pg6.java
+++ CO3/CO3pg6.java
@@ -18,7 +18,7 @@ public static void main(String[] args)
 {  
     System.out.println("I'm a student of SNGCE");  
     System.out.println("Favourite game is BattleGround Mobile India"); 
-	System.out.println("====================");
+    System.out.println("====================");
     System.out.println("Reversed String is:");
     System.out.println("====================");	
     System.out.println(REVERSE.reverseWord("I'm a student of SNGCE"));  
