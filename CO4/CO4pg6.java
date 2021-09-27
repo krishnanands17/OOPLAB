cf420989021556dd0df27ffd0416b51b2b9110df
krishnanands17 <76091927+krishnanands17@users.noreply.github.com>
2021-09-27 18:47:41 +0530

    Add files via upload

---
+++ CO4/CO4pg6.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class CO4pg6 
+{
+static String str;
+public static void main(String ...a) throws Exception 
+{
+Scanner sc = new Scanner(System.in);
+Socket s = new Socket("localhost",9999);
+System.out.println("*****ENTER THE DATA*****");
+Thread.sleep(1000);
+str = sc.next();
+OutputStreamWriter os = new OutputStreamWriter(s.getOutputStream());
+PrintWriter p = new PrintWriter(os);
+os.write(str+"\n");
+os.flush();
+}
+}
